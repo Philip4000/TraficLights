9ef782a75e96445b22ccf613d96a70efda5dd976
Philip4000 <6427676@bk.ru>
2022-11-22 17:23:28 +0300

    Add view's and logic in project

--- TraficLights/ViewController.swift
+++ TraficLights/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var colorViews: [UIView]!
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private var lightOn = 1.0
+    private var lightOff = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
-
+    @IBAction func startButtonDidTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            colorViews[0].alpha = lightOn
+            currentLight = .yellow
+            colorViews[2].alpha = lightOff
+        case .yellow:
+            colorViews[1].alpha = lightOn
+            currentLight = .green
+            colorViews[0].alpha = lightOff
+        case .green:
+            colorViews[2].alpha = lightOn
+            currentLight = .red
+            colorViews[1].alpha = lightOff
+        }
+    }
+    
+    private func setupUI() {
+        colorViews.forEach {
+            $0.layer.cornerRadius = $0.frame.width / 2
+            $0.alpha = lightOff
+        }
+        
+        startButton.layer.cornerRadius = 10
+        startButton.setTitle("START", for: .normal)
+    }
+    
 }
 
